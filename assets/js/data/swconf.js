a191eaca881375d444702d72babf440bc1bd1e6f
JustinChoi21 <JustinChoi21@users.noreply.github.com>
2025-11-14 16:37:29 +0000

    deploy: 8bb5e85ef3a12d4c109ea65d554ee63d58bc6f6c

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1762991778',resources: [
+    cacheName: 'chirpy-1763138236',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
